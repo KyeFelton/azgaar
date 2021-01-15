a28e651afcaa758a91ad232c258f683ad9ee1873
Kye Felton <kye.felton@hotmail.com>
2021-01-15 15:06:04 +1100

    remove semicolons

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,9 +1,9 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Container from "react-bootstrap/Container";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
+import React from "react"
+import logo from "./logo.svg"
+import "./App.css"
+import Container from "react-bootstrap/Container"
+import Navbar from "react-bootstrap/Navbar"
+import Nav from "react-bootstrap/Nav"
 function App() {
   return (<div className="App">
     <Navbar expand="lg" variant="light" bg="info">
@@ -13,9 +13,9 @@ function App() {
           <Nav.Link href="#home">Home</Nav.Link>
           <Nav.Link href="#features">Features</Nav.Link>
           <Nav.Link href="#pricing">Pricing</Nav.Link>
-        </Nav>;
+        </Nav>
       </Container>
-    </Navbar>;
-  </div>);
+    </Navbar>
+  </div>)
 }
 export default App;
